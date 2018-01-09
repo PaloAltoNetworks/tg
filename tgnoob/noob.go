45258645c1bb19bae0a4837cd1388e3d973893db
Christophe Serafin <christophe.serafin@gmail.com>
2018-01-09 15:23:40 -0800

    create certificate authority

--- tgnoob/noob.go
+++ tgnoob/noob.go
@@ -59,6 +59,63 @@ func CreateCA(
 
 }
 
+// CreateSignedCA creates a Certificates Authority Certificate.
+func CreateSignedCA(
+	name string,
+	organization string,
+	password string,
+	signingCertPath string,
+	signingCertKeyPath string,
+	signingCertKeyPass string,
+	out string,
+) (string, string, error) {
+
+	var err error
+
+	if out == "" {
+		if out, err = ioutil.TempDir("", "certificates"); err != nil {
+			return "", "", err
+		}
+	}
+
+	if err = GenerateCertificate(
+		name,                   // string
+		organization,           // commonName
+		password,               // password
+		true,                   // isCA
+		false,                  // authServer
+		false,                  // authClient
+		false,                  // authEmail
+		false,                  // p12
+		"",                     // p12Pass
+		out,                    // out
+		false,                  // force
+		algoECDSA,              // algo
+		signingCertPath,        // signingCertPath
+		signingCertKeyPath,     // signingCertKeyPath
+		signingCertKeyPass,     // signingCertKeyPass
+		[]string{"us"},         // country
+		[]string{"ca"},         // state
+		[]string{"sanjose"},    // city
+		[]string{},             // address
+		[]string{},             // zipCode
+		[]string{organization}, // org
+		[]string{},             // orgUnit
+		[]string{},             // dns
+		[]string{},             // ips
+		24*time.Hour,           // duration
+		[]string{},             // policies
+	); err != nil {
+		return "", "", err
+	}
+
+	certOut := certificatePath(out, name)
+	keyOut := certificateKeyPath(out, name)
+
+	return certOut, keyOut, nil
+
+}
+
 // CreateClientCertificate creates a client certificate.
 func CreateClientCertificate(
 	name string,
