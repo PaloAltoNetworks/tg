49e4a3e985d840c2130e3fca7a6292390a81661f
Philip Hamer <48808444+philipatl@users.noreply.github.com>
2019-12-11 22:12:00 -0500

    provide Windows impl of system cert pool (#19)
    * provide Windows impl of system cert pool
    
    * change GetSystemCertPool to SystemCertPool

---
+++ tglib/certpool.go
@@ -0,0 +1,12 @@
+// +build !windows
+
+package tglib
+
+import (
+	"crypto/x509"
+)
+
+// SystemCertPool gets the system cert pool via Go libraries
+func SystemCertPool() (*x509.CertPool, error) {
+	return x509.SystemCertPool()
+}
